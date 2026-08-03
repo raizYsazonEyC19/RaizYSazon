f6d2379dfed7e70e24e86ef6b2aad55b7747f57d
raizYsazonEyC19 <raizysazonEyC@gmail.com>
2026-08-03 18:02:06 -0500

    Update productos.js

--- productos.js
+++ productos.js
@@ -5,43 +5,51 @@
 const productos = [
 
 {
-    nombre:"Chile Guajillo",
-    categoria:"🌶️ Chiles",
+    nombre:"Achiote condimentado",
+    categoria:"especias",
     descripcion:"Ideal para adobos, caldos y salsas.",
     presentacion:"1 lb • 5 lb • 25 lb",
     imagen:"https://images.unsplash.com/photo-1502741338009-cac2772e18bc?auto=format&fit=crop&w=800&q=80"
 },
 
 {
-    nombre:"Chile Ancho",
-    categoria:"🌶️ Chiles",
-    descripcion:"Excelente para mole y marinados.",
+    nombre:"Achiote molido",
+    categoria:"Especias",
+    descripcion:"Perfecta para sazonar.",
     presentacion:"1 lb • 5 lb • 25 lb",
     imagen:"https://images.unsplash.com/photo-1518977676601-b53f82aba655?auto=format&fit=crop&w=800&q=80"
 },
 
 {
-    nombre:"Canela Entera",
-    categoria:"🧂 Especias",
-    descripcion:"Perfecta para postres y bebidas.",
+    nombre:"Achiote en pasta",
+    categoria:"Especias",
+    descripcion:"Perfecta para sazonar.",
     presentacion:"Bolsa de 1 lb",
     imagen:"https://images.unsplash.com/photo-1596040033229-a9821ebd058d?auto=format&fit=crop&w=800&q=80"
 },
 
 {
-    nombre:"Orégano",
-    categoria:"🌿 Hierbas",
+    nombre:"Acido citrico",
+    categoria:"Especias",
     descripcion:"100% natural.",
     presentacion:"Bolsa de 1 lb",
     imagen:"https://images.unsplash.com/photo-1471193945509-9ad0617afabf?auto=format&fit=crop&w=800&q=80"
 },
 
 {
-    nombre:"Laurel",
-    categoria:"🌿 Hierbas",
+    nombre:"Ajo granulado",
+    categoria:"Especias",
     descripcion:"Ideal para sopas y guisos.",
     presentacion:"Bolsa de 1 lb",
     imagen:"https://images.unsplash.com/photo-1512621776951-a57141f2eefd?auto=format&fit=crop&w=800&q=80"
 }
 
 ];
+
+{
+    nombre: "Pimienta Negra",
+    categoria: "Especias",
+    descripcion: "Ideal para carnes y salsas.",
+    presentacion: "Bolsa de 1 lb",
+    imagen: "https://..."
+},
